0579eef0b696abf82052c09cb451a2c1bf0ccd53
Daniel Dimitrov <daniel@compojoom.com>
2020-06-18 18:05:28 +0200

    Update clientLib to v 0.13.2

--- config.dist.js
+++ config.dist.js
@@ -1,11 +1,9 @@
 import {WALLET_TYPE_IDENTITY} from "trustlines-clientlib/lib-esm/wallets/TLWallet";
 
 export const config = {
-    protocol: "http",
-    wsProtocol: "ws",
-    host: "localhost",
-    port: 5000,
-    path: "api/v1/",
+    relayUrl: "http://localhost:5000/api/v1",
+    messagingUrl: "http://localhost:5000/api/v1",
+    chainId: 17,
     walletType: WALLET_TYPE_IDENTITY,
     identityFactoryAddress: "0x26C8d09E5C0B423E2827844c770F61c9af2870E7",
     identityImplementationAddress: "0x3DD0864668C36D27B53a98137764c99F9FD5B7B2",
