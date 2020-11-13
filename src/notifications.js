0770618b6b7b70b3fd0a750e60dff6c4a7efa83f
Daniel Dimitrov <daniel@compojoom.com>
2020-11-13 10:12:34 +0100

    Send data message
    The trustlines app understands data messages now.

--- src/notifications.js
+++ src/notifications.js
@@ -56,10 +56,33 @@ const init = () => {
 
     const registrationToken = options.token
     const message = {
-        notification: {
-            title: 'Test message title',
-            body: 'Test message body'
-        },
+            "data": {
+                "blockHash": "0x3b6f349e381d165ceaf7813c3059be23e6c41edd7e8224fe5b59d29fa47048a6",
+                "transactionHash": "0xbc8c083c87f78590174f52017e5df6e43ed73510e3e89a265a6602e50f5a7ab1",
+                "logIndex": "0",
+                "blockNumber": "1487062",
+                "messageTitle": "notification 22",
+                "messageBody": "notification+body+is+here 3"
+            },
+            "android": {
+                "priority": "HIGH"
+            },
+            "apns": {
+                "payload": {
+                    "aps": {
+                        "content-available": 1
+                    }
+                },
+                "headers": {
+                    "apns-push-type": "background",
+                    "apns-priority": "5",
+                    "apns-collapse-id": "meykey"
+                }
+            },
+            "notification": {
+                "title": "don#t disply this1",
+                "body": "don#t display this"
+            },
         token: registrationToken
     };
 
